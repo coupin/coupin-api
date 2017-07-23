8d71d190b1eea51bb44c0a8fe735bf6d9ff837bd
Evans Erhobaga-Agofure <evanserhobaga@gmail.com>
2017-07-23 11:46:50 +0100

    db change to server

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
-    url : 'mongodb://127.0.0.1:27017/coupinApp'
+    //url : 'mongodb://127.0.0.1:27017/coupinApp'
     //url : 'mongodb://admin:admin@ds113680.mlab.com:13680/discount-db'
-    //url : 'mongodb://admin:admin@ds127802.mlab.com:27802/coupin-dev'
+    url : 'mongodb://admin:admin@ds127802.mlab.com:27802/coupin-dev'
     //url : 'mongodb://localhost:27017/discount-db'
 }
