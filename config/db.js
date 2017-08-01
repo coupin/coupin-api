19f05eca570bbee0bac377d2943d0f10c485fe15
Evans Erhobaga-Agofure <evanserhobaga@gmail.com>
2017-08-01 21:30:03 +0100

    added heroku mongodb

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
     //url : 'mongodb://127.0.0.1:27017/coupinApp'
     //url : 'mongodb://admin:admin@ds113680.mlab.com:13680/discount-db'
-    url : 'mongodb://admin:admin@ds127802.mlab.com:27802/coupin-dev'
-    //url : 'mongodb://localhost:27017/discount-db'
+    //url : 'mongodb://admin:admin@ds127802.mlab.com:27802/coupin-dev'
+    url : 'mongodb://coupinAdmin:Password1$@ds129593.mlab.com:29593/heroku_ngd9320w'
 }
