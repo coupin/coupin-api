b90daf5a30d079abbdf66950f0921e8df87c45f8
Ralph Olutola <ralph.olutola@unqork.com>
2019-06-07 15:39:35 +0100

    Add env example

--- init.js
+++ init.js
@@ -7,18 +7,18 @@ var Users = require('./app/models/users');
 
 dotenv.config();
 
-var db = process.env.MONGO_URL;
-// var db = process.env.LOCAL_URL;
+// var db = process.env.MONGO_URL;
+var db = process.env.LOCAL_URL;
 
 // const data = require('./seeds/data.json');
 seeder.connect(db, function () {
-    Users.findOne({email: 'admin@coupin.com'}, function (err, user) {
+    Users.findOne({email: process.env.SADMIN}, function (err, user) {
         if (err) {
             Raven.captureException(err);
             process.exit(0);
         } else if (!user) {
             var admin = new Users();
-            admin.email = process.env.ADMINEMAIL;
+            admin.email = process.env.SADMIN;
             admin.password = 'coupinapp';
             admin.role = 0;
 
@@ -28,11 +28,11 @@ seeder.connect(db, function () {
                     process.exit(0);
                 }
 
-                console.log(chalk.blue.bold('Done with creating the customer'));
+                console.log(chalk.blue.bold('Done with creating the Admin'));
                 process.exit(0);
             });
         } else {
-            console.log(chalk.blue.bold('Customer already exists.'));
+            console.log(chalk.blue.bold('Admin already exists.'));
             process.exit(0);
         }
     });
